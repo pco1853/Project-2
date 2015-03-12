4c857e34aaa81d49a1e3d79b518c6ffbef5b2956
Student <student@c2025m-16.main.ad.rit.edu>
2015-03-12 10:09:50 -0400

    Initial Commit

---
+++ DataDownloader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataDownloader
+//
+//  Created by Student on 3/12/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
